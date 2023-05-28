5857f5cfddecb336ce4bf72d8438bcd45ad580d4
Gitanjali08 <118281982+Gitanjali08@users.noreply.github.com>
2023-05-28 20:50:28 +0530

    Add files via upload

--- assignmentf6.c
+++ assignmentf6.c
@@ -1,77 +1,47 @@
 #include <stdio.h>
-
-#define MAX_FRAMES 3   // Maximum number of frames
-
-void printFrames(int frames[], int numFrames) {
-    for (int i = 0; i < numFrames; i++) {
-        if (frames[i] == -1)
-            printf("- ");
-        else
-            printf("%d ", frames[i]);
-    }
-    printf("\n");
-}
-
-int isPageInMemory(int page, int frames[], int numFrames) {
-    for (int i = 0; i < numFrames; i++) {
-        if (frames[i] == page)
-            return 1;
-    }
-    return 0;
-}
-
-int getOldestPageIndex(int pages[], int numPages, int frames[], int numFrames, int currentIndex) {
-    int oldestIndex = currentIndex;
-    for (int i = 0; i < numFrames; i++) {
-        int j;
-        for (j = currentIndex - 1; j >= 0; j--) {
-            if (frames[i] == pages[j])
-                break;
-        }
-        if (j == -1) {
-            oldestIndex = i;
-            break;
-        }
-        if (j < oldestIndex)
-            oldestIndex = i;
-    }
-    return oldestIndex;
-}
-
-int main() {
-    int pages[] = {1, 2, 3, 4, 5, 6, 1, 2, 3, 7, 8};  // Page reference string
-    int numPages = sizeof(pages) / sizeof(pages[0]);
-
-    int frames[MAX_FRAMES];   // Array to hold page frames
-    int numFrames = 0;         // Number of page frames currently occupied
-
-    // Initialize frames as empty
-    for (int i = 0; i < MAX_FRAMES; i++)
-        frames[i] = -1;
-
-    // Perform FIFO page replacement
-    int pageFaults = 0;
-    int currentIndex = 0;   // Current index in the page reference string
-    for (int i = 0; i < numPages; i++) {
-        printf("Page %d: ", pages[i]);
-        if (!isPageInMemory(pages[i], frames, numFrames)) {
-            if (numFrames < MAX_FRAMES) {
-                frames[numFrames] = pages[i];
-                numFrames++;
-            } else {
-                int oldestPageIndex = getOldestPageIndex(pages, numPages, frames, numFrames, currentIndex);
-                frames[oldestPageIndex] = pages[i];
-            }
-            pageFaults++;
-            printFrames(frames, numFrames);
-        } else {
-            printf("Page hit!\n");
-        }
-        currentIndex++;
-    }
-
-    printf("\nTotal page faults: %d\n", pageFaults);
-
-    return 0;
-}
-
+    int main()  
+    {  
+        int incomingStream[] = {1, 2, 3, 4, 5, 6, 1, 2, 3, 7, 8};  
+        int pageFaults = 0;  
+        int frames = 3;  
+        int m, n, s, pages;   
+        pages = sizeof(incomingStream)/sizeof(incomingStream[0]);   
+        printf(" Incoming \t Frame 1 \t Frame 2 \t Frame 3 ");  
+        int temp[ frames ];  
+        for(m = 0; m < frames; m++)  
+        {  
+            temp[m] = -1;  
+        }  
+        for(m = 0; m < pages; m++)  
+        {  
+            s = 0;   
+            for(n = 0; n < frames; n++)  
+            {  
+                if(incomingStream[m] == temp[n])  
+                {  
+                    s++;  
+                    pageFaults--;  
+                }  
+            }  
+            pageFaults++;  
+            if((pageFaults <= frames) && (s == 0))  
+            {  
+                temp[m] = incomingStream[m];  
+            }  
+            else if(s == 0)  
+            {  
+                temp[(pageFaults - 1) % frames] = incomingStream[m];  
+            }  
+            printf("\n");  
+            printf("%d\t\t\t",incomingStream[m]);  
+            for(n = 0; n < frames; n++)  
+            {  
+                if(temp[n] != -1)  
+                    printf(" %d\t\t\t", temp[n]);  
+                else  
+                    printf(" - \t\t\t");  
+            }  
+        }  
+        printf("\nTotal Page Faults:\t%d\n", pageFaults);  
+        return 0;  
+    }  
